df2a8f2e7be4ef9aed6b28526a124866372c3e41
Daniel Ochoa <daniel.o.ochoa@gmail.com>
2020-07-06 09:30:56 -0500

    working lexer (tokenizer)

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/danielochoa/grendel
+
+go 1.12
+
+require rsc.io/quote v1.5.2
---
+++ token/token.go
@@ -0,0 +1,70 @@
+package token
+
+// TokenType Any given token type, see below in constants.
+type TokenType string
+
+// Token holds any given AST token, for lexing, we store character like variables or
+// reserved words as a type itself
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers & literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 123456
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	LT       = "<"
+	GT       = "<"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+// LookupIdent - searches the keywords map to know if our ident is a keyword or some var name
+// TODO: Case insensitive find
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
